f41be4e892d7e04e7ba66e6a76b7b896a3cd991f
Ian Robert O'Brien <13039736+ianrobrien@users.noreply.github.com>
2023-08-31 09:02:07 +0200

    Fix hostname

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -1,5 +1,5 @@
 resource "digitalocean_droplet" "fakebank_droplet" {
-  name      = "fakebank_droplet"
+  name      = "fakebank-droplet"
   size      = "s-1vcpu-512mb-10gb"
   image     = "ubuntu-22-04-x64"
   region    = "ams3"
