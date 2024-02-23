fdaccfcc66497baa189b300908257abb2667bba4
Ian Robert O'Brien <13039736+ianrobrien@users.noreply.github.com>
2024-02-23 10:26:40 +0100

    Add tests to account mapper

--- fakebank-impl/src/test/java/no/obrien/fakebank/repository/AccountRepositoryMockTest.java
+++ fakebank-impl/src/test/java/no/obrien/fakebank/repository/AccountRepositoryMockTest.java
@@ -1,5 +1,6 @@
 package no.obrien.fakebank.repository;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -21,5 +22,82 @@ public class AccountRepositoryMockTest {
   void getAccount_invalidAccountId_returnsNoAccount() {
     var account = accountRepositoryMock.findById(-1L);
     assertTrue(account.isEmpty());
+  }
+
+  @Test
+  void save() {
+    assertThrows(
+        UnsupportedOperationException.class,
+        () -> accountRepositoryMock.save(null));
+  }
+
+  @Test
+  void saveAll() {
+    assertThrows(
+        UnsupportedOperationException.class,
+        () -> accountRepositoryMock.saveAll(null));
+  }
+
+  @Test
+  void existsById() {
+    assertThrows(
+        UnsupportedOperationException.class,
+        () -> accountRepositoryMock.existsById(null));
+  }
+
+  @Test
+  void findAll() {
+    assertThrows(
+        UnsupportedOperationException.class,
+        accountRepositoryMock::findAll);
+  }
+
+  @Test
+  void findAllById() {
+    assertThrows(
+        UnsupportedOperationException.class,
+        () -> accountRepositoryMock.findAllById(null));
+  }
+
+  @Test
+  void count() {
+    assertThrows(
+        UnsupportedOperationException.class,
+        accountRepositoryMock::count);
+  }
+
+  @Test
+  void deleteById() {
+    assertThrows(
+        UnsupportedOperationException.class,
+        () -> accountRepositoryMock.deleteById(null));
+  }
+
+  @Test
+  void delete() {
+    assertThrows(
+        UnsupportedOperationException.class,
+        () -> accountRepositoryMock.delete(null));
+  }
+
+  @Test
+  void deleteAllById() {
+    assertThrows(
+        UnsupportedOperationException.class,
+        () -> accountRepositoryMock.deleteAllById(null));
+  }
+
+  @Test
+  void deleteAll() {
+    assertThrows(
+        UnsupportedOperationException.class,
+        accountRepositoryMock::deleteAll);
+  }
+
+  @Test
+  void testDeleteAll() {
+    assertThrows(
+        UnsupportedOperationException.class,
+        () -> accountRepositoryMock.deleteAll(null));
   }
 }
