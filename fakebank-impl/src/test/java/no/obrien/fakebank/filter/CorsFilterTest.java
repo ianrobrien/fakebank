17e299b65b44d00539935d42871906f538de7886
Ian Robert O'Brien <13039736+ianrobrien@users.noreply.github.com>
2024-03-14 20:13:10 +0100

    Add tests to corsfilter

---
+++ fakebank-impl/src/test/java/no/obrien/fakebank/filter/CorsFilterTest.java
@@ -0,0 +1,50 @@
+package no.obrien.fakebank.filter;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockFilterChain;
+import org.springframework.mock.web.MockFilterConfig;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+@SpringBootTest
+class CorsFilterTest {
+
+  @Test
+  void init() {
+    assertDoesNotThrow(() -> {
+      var corsFilter = new CorsFilter();
+      corsFilter.init(new MockFilterConfig());
+    });
+  }
+
+  @Test
+  void doFilter() {
+    assertDoesNotThrow(() -> {
+      var response = new MockHttpServletResponse();
+
+      var corsFilter = new CorsFilter();
+      corsFilter.doFilter(
+          new MockHttpServletRequest(),
+          response,
+          new MockFilterChain());
+
+      assertEquals("*", response.getHeader("Access-Control-Allow-Origin"));
+      assertEquals("GET, POST, PUT, DELETE", response.getHeader("Access-Control-Allow-Methods"));
+      assertEquals("*", response.getHeader("Access-Control-Allow-Headers"));
+      assertEquals("true", response.getHeader("Access-Control-Allow-Credentials"));
+      assertEquals("3600", response.getHeader("Access-Control-Max-Age"));
+    });
+  }
+
+  @Test
+  void destroy() {
+    assertDoesNotThrow(() -> {
+      var corsFilter = new CorsFilter();
+      corsFilter.destroy();
+    });
+  }
+}
