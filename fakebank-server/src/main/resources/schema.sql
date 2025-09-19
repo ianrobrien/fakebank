e32f4a6f4f11994702fa6d7d6eed61252dc8e034
Ian Robert O'Brien <13039736+ianrobrien@users.noreply.github.com>
2025-09-19 14:29:00 +0200

    Add super linter

--- fakebank-server/src/main/resources/schema.sql
+++ fakebank-server/src/main/resources/schema.sql
@@ -1,17 +1,17 @@
 CREATE TABLE IF NOT EXISTS owner
 (
-  id         BIGINT       NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(255) NOT NULL,
-  last_name  VARCHAR(255) NOT NULL,
-  PRIMARY KEY (id)
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    first_nameVARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS account
 (
-  id       BIGINT       NOT NULL AUTO_INCREMENT,
-  owner_id BIGINT       NOT NULL,
-  balance  DOUBLE       NOT NULL,
-  currency VARCHAR(255) NOT NULL,
-  FOREIGN KEY (owner_id) REFERENCES owner (id),
-  PRIMARY KEY (id)
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    owner_id BIGINT NOT NULL,
+    balance DOUBLE NOT NULL,
+    currency VARCHAR(255) NOT NULL,
+    FOREIGN KEY (owner_id) REFERENCES owner (id),
+    PRIMARY KEY (id)
 );
